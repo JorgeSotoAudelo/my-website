9689d344527b2894cc017cdb9df51d1bd641136c
Jorge Luis Soto Audelo <JorgeSotoAudelo@gmail.com>
2026-05-31 14:53:21 -0700

    backend pipeline test

--- backend/websiteapi/src/main/java/com/jorgesoto/websiteapi/WebsiteapiApplication.java
+++ backend/websiteapi/src/main/java/com/jorgesoto/websiteapi/WebsiteapiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebsiteapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebsiteapiApplication.class, args); 
+		SpringApplication.run(WebsiteapiApplication.class, args);
 	}
 
 }
